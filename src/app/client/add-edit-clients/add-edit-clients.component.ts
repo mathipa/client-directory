bc0bccdf689a8e2fa72a417d47b28e16bbd63129
mathipa87@gmail.com <mathipa87@gmail.com>
2020-10-21 00:46:55 +0200

    Edit and createn component

--- src/app/client/add-edit-clients/add-edit-clients.component.ts
+++ src/app/client/add-edit-clients/add-edit-clients.component.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { SharedService } from 'src/app/shared.service';
 
 @Component({
-  selector: 'app-add-edit-clients',
+  selector: 'add-edit-clients',
   templateUrl: './add-edit-clients.component.html',
   styleUrls: ['./add-edit-clients.component.scss']
 })
 export class AddEditClientsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: SharedService) { }
+
+  @Input()clientdata: any;
+  ClientId: string;
+  FirstName: string;
+  LastName: string;
+  MobileNumber: string;
+  IdNumber: string;
+  PhysicalAddress: string;
 
   ngOnInit(): void {
+    this.loadClients();
+  }
+
+
+  loadClients (){
+    this.ClientId = this.clientdata.ClientId;
+    this.FirstName = this.clientdata.FirstName;
+    this.LastName = this.clientdata.LastName;
+    this.MobileNumber = this.clientdata.MobileNumber;
+    this.IdNumber = this.clientdata.IdNumber;
+    this.PhysicalAddress = this.clientdata.PhysicalAddress;
+
+  }
+  addClient(){
+    var val = {
+      ClientId:this.ClientId,
+      FirstName:this.FirstName,
+      LastName:this.LastName,
+      MobileNumber: this.MobileNumber,
+      IdNumber: this.IdNumber,
+      PhysicalAddress: this.PhysicalAddress
+    };
+    this.service.addClient(val).subscribe(res=>{
+      alert(res.toString());
+    });
+  }
+
+  updateClient(){
+    var val = {
+      ClientId:this.ClientId,
+      FirstName:this.FirstName,
+      LastName:this.LastName,
+      MobileNumber: this.MobileNumber,
+      IdNumber: this.IdNumber,
+      PhysicalAddress: this.PhysicalAddress};
+    this.service.UpdateClient(val).subscribe(res=>{
+    alert(res.toString());
+    });
   }
 
 }
--- src/app/client/list-clients/list-clients.component.ts
+++ src/app/client/list-clients/list-clients.component.ts
@@ -12,13 +12,70 @@ export class ListClientsComponent implements OnInit {
 
   ClientList: any[];
 
+  ModalTitle: string;
+  ActivateAddEditClient:boolean=false;
+  clientdata:any;
+
+  ClientIdFilter: string="";
+  ClientFilterName: string="";
+  ClientListWithoutFilter: any[];
+
   ngOnInit(): void {
+    this.getClientList();
+  }
+
+  addClient() {
+    this.clientdata = {
+      ClientId: 0,
+      FirstName:"",
+      LastName:"",
+      MobileNumber:"",
+      IdNumber:"",
+      PhysicalAddress:""
+    }
+    this.ModalTitle = "Add Client";
+    this.ActivateAddEditClient = true;
+  }
+
+  editClient(item) {
+    this.clientdata = item;
+    this.ModalTitle = "Edit Client";
+    this.ActivateAddEditClient = true;
+  }
+
+  deleteClient(item) {
+    if(confirm('Are you sure?')) {
+      this.service.DeleteClient(item.ClientId).subscribe(data=> {
+        alert(data.toString());
+        this.getClientList();
+      });
+    }
+  
+  }
+
+  closeModal() {
+    this.ActivateAddEditClient=false;
     this.getClientList();
   }
 
   getClientList() {
     this.service.getClintList().subscribe(data=>{
       this.ClientList = data;
+    });
+  }
+
+
+  FilterFn(){
+    var ClientIdFilter = this.ClientIdFilter;
+    var ClientNameFilter = this.ClientFilterName;
+
+    this.ClientList = this.ClientListWithoutFilter.filter(function (el){
+        return el.ClientId.toString().toLowerCase().includes(
+          ClientIdFilter.toString().trim().toLowerCase()
+        )&&
+        el.FirstName.toString().toLowerCase().includes(
+          ClientNameFilter.toString().trim().toLowerCase()
+        )
     });
   }
 
